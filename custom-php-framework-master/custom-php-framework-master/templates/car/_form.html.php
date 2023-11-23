523a5317021526bf02129d2a3771af9abcacee28
Ishija <pwl.nowosielski@gmail.com>
2023-11-23 19:07:36 +0100

    lab f

---
+++ custom-php-framework-master/custom-php-framework-master/templates/car/_form.html.php
@@ -0,0 +1,18 @@
+<?php
+    /** @var $car ?\App\Model\Car */
+?>
+
+<div class="form-group">
+    <label for="brand">Brand</label>
+    <input type="text" id="brand" name="car[brand]" value="<?= $car ? $car->getBrand() : '' ?>">
+</div>
+
+<div class="form-group">
+    <label for="description">Description</label>
+    <textarea id="description" name="car[description]"><?= $car? $car->getDescription() : '' ?></textarea>
+</div>
+
+<div class="form-group">
+    <label></label>
+    <input type="submit" value="Submit">
+</div>
